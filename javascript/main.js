8e7df18f330fa96b4d8ab3853a7d7d9e079b6bb6
Yagnik Vadher <yagniktechmail@gmail.com>
2017-11-24 01:10:10 -0800

    Added a singel timer

--- javascript/main.js
+++ javascript/main.js
@@ -34,15 +34,10 @@ var Debbie = new User(gameMap);
 //So apprentlly it has to be in window.onload and then setinterval has to be in clouser
 // SO function () { out function !!!}
 window.onload = function() {
-  let player1 = setInterval(function(){ John.randomMove(gameMap); }  ,100);
-  let player2 = setInterval( function() {Sally.randomMove(gameMap); } ,100);
-  let player3 = setInterval( function() {Debbie.randomMove(gameMap); } ,100);
-  let draw = setInterval(function() { gameMap.draw(); }  , 110);
-
+  let mainTimer= setInterval(function(){
+    John.randomMove(gameMap);
+    Sally.randomMove(gameMap);
+    Debbie.randomMove(gameMap);
+     gameMap.draw();
+   }  , 150);
 }
-
-
-
-
-
-// //gameMap.init();
